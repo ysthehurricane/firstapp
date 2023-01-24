3a9fcfbb4e85d1e055ad5a5881f4ff6cdd867995
Yash Patel <55491822+ysthehurricane@users.noreply.github.com>
2023-01-24 20:05:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,5 @@ app = FastAPI()
 async def index(request: Request):
     return HTMLResponse("<h1>Yash Patel</h1>")
 
-
-
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
-
-# from flask import Flask
-
-# @app.route('/', methods=["GET"])
-# def index():
-#     return "yash patel"
-
-# app = Flask(__name__)
-
-# if __name__ == '__main__':
-#     app.run(debug=False)
